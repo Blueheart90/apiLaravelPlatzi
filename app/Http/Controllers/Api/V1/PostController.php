3c4440f84f833d6ff0fb9275c9eaa4deae793c05
Jesus david <chuchober@hotmail.com>
2022-06-08 08:43:37 -0500

    Add feat to delete post

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -62,6 +62,7 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
-        //
+        $post->delete();
+        return response()->json(['message' => 'Post deleted successfully'], 204);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('v1/posts', PostController::class)->only(['index', 'show']);
+Route::apiResource('v1/posts', PostController::class)->only(['index', 'show', 'destroy']);
